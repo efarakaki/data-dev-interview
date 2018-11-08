e58f5d2018dd1dd8fb3e0361b82e214b2d873dbc
Santiago Cabrera <scabrera@tiendanube.com>
2018-11-08 20:07:36 -0300

    Schema push

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE data-tech-interview;
+
+CREATE TABLE `films_by_year` (
+  `year` int(11) NOT NULL,
+  `rating` varchar(100) NOT NULL,
+  `count` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
