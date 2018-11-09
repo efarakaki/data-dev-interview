1dfe1b6b5f0486fe6cae8ab1c3b0019dd85a8d5a
Ernesto <ernesto.arakaki@mercadolibre.com>
2018-11-09 11:54:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import gzip
+import csv
 
 def main():
     print('Welcome to data tech interview 1')
+    with open('dataset/basics.tsv', 'r') as file:
+        readf = csv.DictReader(file, dialect='excel-tab')
+        contador = 0
+        for reg in readf:
+            if contador < 10:
+                print(reg[0])
+            else:
+                break
+            contador = contador +1
+
 
 
 if __name__ == "__main__":
